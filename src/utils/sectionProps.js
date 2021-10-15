f2695cc0ea890d3dc18ea03a4cd28763227d313f
Win Nebergall <anatolloflint@gmail.com>
2021-10-15 16:56:14 +1000

    sizes panel added

--- src/utils/sectionProps.js
+++ src/utils/sectionProps.js
@@ -41,7 +41,7 @@ export const SidePanelProps = {
     // VectorSection,
     // SizeSection,
     // BackgroundSection,
-    // SizesSection,
+    SizesSection,
   ],
   defaultSection: "resources",
 };
